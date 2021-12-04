82d72242c65b5ae19d5a8bc01ac822819f7193f4
supertgo <thiagoroberto2003@gmail.com>
2021-12-04 11:56:56 -0300

    Adding configuration files and creating index home page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>Dashgo</title>
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
