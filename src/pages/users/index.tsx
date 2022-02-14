e7938671c2b88fd261019d84f6aee6b64cbb5392
supertgo <thiagoroberto2003@gmail.com>
2022-02-14 09:41:07 -0300

    feat(api): :sparkles: add mirage js and created fake data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,12 +22,19 @@ import Sidebar from 'components/Sidebar';
 import Pagination from 'components/Pagination';
 
 import { RiAddLine } from 'react-icons/ri';
+import { useEffect } from 'react';
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <Box>
