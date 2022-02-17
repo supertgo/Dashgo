3010b23479847249727c0b6b6def2f523048598b
supertgo <thiagoroberto2003@gmail.com>
2022-02-17 13:16:03 -0300

    feat(api): :sparkles: using mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -19,6 +19,11 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
+import { useMutation } from 'react-query';
+import { api } from 'services/api';
+import { queryClient } from 'services/queryClient';
+import { useRouter } from 'next/router';
+
 type CreateUserFormData = {
   name: string;
   email: string;
@@ -39,6 +44,24 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user: {
+          ...user,
+          created_at: new Date()
+        }
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users');
+      }
+    }
+  );
   const {
     register,
     handleSubmit,
@@ -46,14 +69,14 @@ export default function CreateUser() {
   } = useForm({
     resolver: yupResolver(createUserFormSchema)
   });
-
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
     values
   ) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await createUser.mutateAsync(values);
 
-    console.log(values);
+    router.push('/users');
   };
+
   return (
     <Box>
       <Header />
